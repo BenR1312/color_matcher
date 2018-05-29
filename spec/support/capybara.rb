2a22a9fa36e04d325492a52740997830b087d54c
Benjamin Robinson <benjaminrobinson1313@gmail.com>
2018-05-29 18:49:32 +0800

    Update capybara with chrome driver and selenium setup.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,1 +1,38 @@
-Capybara.asset_host = 'http://localhost:3000'
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'selenium/webdriver'
+
+# By default Capybara will use Selenium+Firefox for `js:true` feature specs.
+# The following lines will tell Capybara to use Chrome instead of Firefox,
+Capybara.register_driver :selenium do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(--window-size=1280,1024) },
+    loggingPrefs: { browser: 'ALL' }
+  )
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+  )
+end
+
+# Added browser logging to headless chrome, to read browser error logs please use:
+# page.driver.browser.manage.logs.get(:browser)
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu no-sandbox --window-size=1280,1024) },
+    loggingPrefs: { browser: 'ALL' }
+  )
+
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options: browser_options,
+    desired_capabilities: capabilities
+  )
+end
+
+Capybara.javascript_driver = :headless_chrome
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,3 +1,15 @@
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    begin
+      DatabaseCleaner.start
+      # Test factories in spec/factories are working.
+
+      # TODO: Enable this when loaded core factories on ms_mortgage
+      # FactoryBot.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
 end
